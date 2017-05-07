8887c2b0f1819edbf8d989ae64a18e01a5819883
Dylan Sweetensen <dylansweetensen@gmail.com>
2017-05-07 12:52:29 +1200

    Adding option to serve single page application

--- dist/index.js
+++ dist/index.js
@@ -5,13 +5,17 @@ var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
 var _commander = require('commander');
 
 var _commander2 = _interopRequireDefault(_commander);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_commander2.default.version('0.0.1').usage('[options] [directory]').option('-p, --port [port]', 'Specify port [8888]', '8888').option('-i, --index [index]', 'Specify index file [index.html]', 'index.html').parse(process.argv);
+_commander2.default.version('0.0.5').usage('[options] [directory]').option('-s, --single', 'Only serve index', false).option('-p, --port [port]', 'Specify port [8888]', '8888').option('-i, --index [index]', 'Specify index file [index.html]', 'index.html').parse(process.argv);
 
 var app = (0, _express2.default)();
 
@@ -22,12 +26,19 @@ if (!directory) {
 }
 
 var port = _commander2.default.port,
-    index = _commander2.default.index;
+    index = _commander2.default.index,
+    single = _commander2.default.single;
 
 
 app.use(_express2.default.static(directory, {
   index: index
 }));
+
+if (single) {
+  app.get('*', function (req, res) {
+    res.sendFile(_path2.default.resolve(__dirname, directory, index));
+  });
+}
 
 app.listen(port, '127.0.0.1', function () {
   console.log('Listening on: http://127.0.0.1:' + port);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
 import express from 'express';
+import path from 'path';
 import commander from 'commander';
 
 commander
   .version('0.0.5')
   .usage('[options] [directory]')
+  .option('-s, --single', 'Only serve index', false)
   .option('-p, --port [port]', 'Specify port [8888]', '8888')
   .option('-i, --index [index]', 'Specify index file [index.html]', 'index.html')
   .parse(process.argv);
@@ -21,11 +23,18 @@ if (!directory) {
 const {
   port,
   index,
+  single,
 } = commander;
 
 app.use(express.static(directory, {
   index,
 }));
+
+if (single) {
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, directory, index));
+  });
+}
 
 app.listen(port, '127.0.0.1', () => {
   console.log(`Listening on: http://127.0.0.1:${port}`);
